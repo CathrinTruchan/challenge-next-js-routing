f9780589d98bd41fc82fb0e2206319e1248329ba
Cathrin Truchan <cathrin.truchan@gmail.com>
2022-10-05 11:29:49 +0200

    add routing to header, index.js and account.js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,33 @@
+import Link from "next/link";
 import styled from "styled-components";
+import StyledNavLink from "./StyledNavLink";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const { pathname } = useRouter();
   return (
     <StyledHeader>
       <StyledNav>
-        <a>Home</a>
-        <a>Product</a>
-        <a>Team</a>
-        <a>Account</a>
+        <Link href="/" passHref>
+          <StyledNavLink active={pathname === "/" ? true : false}>
+            Home
+          </StyledNavLink>
+        </Link>
+        <Link href="/product" passHref>
+          <StyledNavLink active={pathname === "/product" ? true : false}>
+            Product
+          </StyledNavLink>
+        </Link>
+        <Link href="/team" passHref>
+          <StyledNavLink active={pathname === "/team" ? true : false}>
+            Team
+          </StyledNavLink>
+        </Link>
+        <Link href="/account" passHref>
+          <StyledNavLink active={pathname === "/account" ? true : false}>
+            Account
+          </StyledNavLink>
+        </Link>
       </StyledNav>
     </StyledHeader>
   );
